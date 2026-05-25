b0caf973d4b684eff11398e9bdfc206e13ad3749
Raúl Brito <radulito19@gmail.com>
2026-05-25 19:06:11 -0400

    Cell Data type

--- SelectionBox/sb.gd
+++ SelectionBox/sb.gd
@@ -84,21 +84,18 @@ func handle_movement():
 		show_move(cell_pos,ramsa.movement)
 
 func show_move(cell:Vector2i,amount:int):
-	remove_tiles()
-	is_movement = true
-	var all_tiles = retrieve_movement_tiles(cell,amount)
-	var floor_data = floor_1.get_floor_data(true)
-	var valid_cells = [cell_pos]
-	for each in floor_data:
-		if all_tiles.has(each.cell):
-			valid_cells.append(each.cell)
-	
-	for each in valid_cells:
-		create_tile(each)
-
-func retrieve_movement_tiles(start_cell:Vector2i,amount:int):
-	print(floor_1.get_available_surrounding_cells(start_cell,true))
-	return floor_1.get_available_surrounding_cells(start_cell,true)
+	pass
+	#remove_tiles()
+	#is_movement = true
+	#var all_tiles = retrieve_movement_tiles(cell,amount)
+	#var floor_data = floor_1.get_floor_data(true)
+	#var valid_cells = [cell_pos]
+	#for each in floor_data:
+		#if all_tiles.has(each.cell):
+			#valid_cells.append(each.cell)
+	#
+	#for each in valid_cells:
+		#create_tile(each)
 
 func create_tile(cell:Vector2i):
 	var new_instance = MOVE_TILE.instantiate()
@@ -118,57 +115,3 @@ func remove_tiles():
 	is_movement = false
 	for child in tiles_ctn.get_children():
 		child.queue_free()
-
-func line_vertical(max_tile:Vector2,num:int):
-	return Vector2(
-			max_tile.x - num,
-			max_tile.y + num,
-		)
-
-func line_horizontal(max_tile:Vector2,num:int):
-	return Vector2(
-			max_tile.x + num,
-			max_tile.y + num,
-		)
-
-
-
-
-func retrieve_movement_tiles_old(cell:Vector2i,amount:int):
-	if amount == 0:
-		return [cell_pos]
-		
-	
-	var max_left = Vector2(cell_pos.x,cell_pos.y - amount)
-	var max_right = Vector2(cell_pos.x,cell_pos.y + amount)
-	var max_top = Vector2(cell_pos.x + amount,cell_pos.y)
-	var max_down = Vector2(cell_pos.x - amount,cell_pos.y)
-	
-	var maxed = [max_left,max_right,max_top,max_down]
-	for each in (amount - 1):
-		var num = each + 1
-		maxed.append(line_vertical(max_left,num))
-		maxed.append(line_vertical(max_top,num))
-		maxed.append(line_horizontal(max_left,num))
-		maxed.append(line_horizontal(max_down,num))
-	
-	
-	#for each in maxed:
-		#create_tile(each)
-	maxed.append_array(retrieve_movement_tiles_old(cell,amount-1))
-	return maxed
-
-
-func four_directions(cell,visited):
-	var left  = Vector2(cell.x, cell.y + 1)
-	var right = Vector2(cell.x, cell.y - 1)
-	var top   = Vector2(cell.x + 1, cell.y)
-	var down  = Vector2(cell.x - 1, cell.y)
-	
-	var results = []
-			
-	for each in [left,right,top,down]:
-		if !visited.has(each):
-			results.append(each)
-	
-	return results
---
+++ World/floor_1.gd
@@ -0,0 +1,103 @@
+extends TileMapLayer
+
+class Cell_Data:
+	var cell:Vector2i
+	var position:Vector2
+	var walkable:bool
+	var elevation:int
+	func _init(_cell,_position,_walkable,_elevation) -> void:
+		cell     = _cell
+		position = _position
+		walkable = _walkable
+		elevation= _elevation
+		
+
+var astar = AStarGrid2D.new()
+
+func setup_grid():
+	# 1. Define the grid size boundary (Rect2i)
+	astar.region = get_used_rect()
+	
+	# 2. Define sizing (Optional, default is Vector2(1,1))
+	# Keep this as (1, 1) if you want paths returned as grid coordinates (e.g., Vector2i(4, 5)).
+	# Match your tile size (e.g., Vector2(64, 32)) if you want paths returned in pixel coordinates.
+	astar.cell_size = Vector2(32, 16) 
+	
+	# 3. Choose how diagonals behave
+	# DIAGONAL_MODE_NEVER: 4-directional movement (Up, Down, Left, Right)
+	# DIAGONAL_MODE_ALWAYS: 8-directional movement (Allows cutting corners)
+	astar.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
+	
+	# 4. Choose the distance scoring math (Heuristics)
+	# HEURISTIC_MANHATTAN: Perfect for 4-directional/Tactical games (calculates L-shapes).
+	# HEURISTIC_EUCLIDEAN: Perfect for 8-directional/Free movement (calculates straight lines).
+	astar.default_compute_heuristic = AStarGrid2D.HEURISTIC_MANHATTAN
+	astar.default_estimate_heuristic = AStarGrid2D.HEURISTIC_MANHATTAN
+	
+	#4.5 Set obstacles
+	var all_cells = get_floor_data()
+	for cell in all_cells:
+		if !cell.walkable:
+			print(cell.cell)
+			astar.set_point_solid(cell.cell, true)
+	# 5. Bake the configuration! (CRITICAL STEP)
+	astar.update()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	setup_grid()
+	#print(get_movement_route(Vector2i(-5,12),Vector2i(-4,14)))
+	pass
+
+
+func get_cell_data(cell:Vector2) ->Cell_Data:
+	var cell_pos = map_to_local(cell)
+	var elevation = get_cell_tile_data(Vector2(cell)).get_custom_data('Elevation')
+	var pos = Vector2(cell_pos.x,cell_pos.y + (-8 * elevation))
+	var walkable = get_cell_tile_data(Vector2(cell)).get_custom_data('Walkable')
+	return Cell_Data.new(cell,pos,walkable,elevation)
+	#return {
+			#"cell":cell,
+			#'walkable':get_cell_tile_data(Vector2(cell)).get_custom_data('Walkable'),
+			#'elevation':elevation,
+			#'position':Vector2(
+				#pos.x,
+				#pos.y + (-8 * elevation)
+			#)
+		#}
+
+func get_floor_data(only_walkable:bool=false):
+	var floor_data=[]
+	for cell in get_used_cells():
+		var cell_data = get_cell_data(cell)
+		if only_walkable && get_cell_tile_data(Vector2(cell)).get_custom_data('Walkable'):
+			floor_data.append(cell_data)
+		elif !only_walkable: floor_data.append(cell_data)
+	return floor_data
+
+func get_available_surrounding_cells(cell:Vector2i,outofbound=false):
+	var surr_cells = get_surrounding_cells(cell)
+	var floor_data = get_floor_data()
+	var avaliable_surrounding_cells = []
+	for _cell in floor_data:
+		if surr_cells.has(_cell.cell):
+			if outofbound:
+				if astar.is_in_boundsv(_cell.cell):
+					avaliable_surrounding_cells.append(_cell)
+			else:
+				avaliable_surrounding_cells.append(_cell)
+		
+	
+	
+	return avaliable_surrounding_cells
+
+
+func get_movement_route(my_grid_pos: Vector2i, target_grid_pos: Vector2i):
+	if not astar.is_in_bounds(target_grid_pos.x, target_grid_pos.y):
+		return []
+	if astar.is_point_solid(target_grid_pos):
+		return []
+		
+	# Get clean grid index array paths
+	var grid_path: Array[Vector2i] = astar.get_id_path(my_grid_pos, target_grid_pos)
+	return grid_path
